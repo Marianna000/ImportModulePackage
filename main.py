2663cb150a4769c95c924ebf66b7405914b0d4f1
Your Name <marinabananas@gmail.com>
2023-08-24 12:24:37 +0300

    -first

---
+++ main.py
@@ -0,0 +1,16 @@
+from application.db import people
+from application import salary
+import datetime
+
+import env as env
+
+if __name__ == '__main__':
+
+    print (datetime.date.today())
+    def calculate_salary()
+    def get_employees()
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
